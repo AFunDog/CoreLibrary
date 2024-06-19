b7aa97bcb9c213f5ab48c2d00a4e573110c87d77
Zeng Kun <1489782679@qq.com>
2024-06-19 14:10:53 +0800

    1.0.46 1.新增集合增删绑定

--- CoreServices/DataBinding/DataBindingService.cs
+++ CoreServices/DataBinding/DataBindingService.cs
@@ -3,7 +3,9 @@ using CoreServices.DataBinding.Structs;
 using CoreServices.DataBinding.ValueConverters;
 using CoreServices.Template;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -16,6 +18,7 @@ namespace CoreServices.DataBinding
     {
         // source sourceProperty bindingInfo
         private readonly Dictionary<INotifyPropertyChanged, Dictionary<PropertyInfo, List<BindingInfo>>> _bindings = [];
+        private readonly Dictionary<INotifyCollectionChanged, List<(Action<INotifyCollectionChanged, IList> itemsAdded, Action<INotifyCollectionChanged, IList> itemsRemoved)>> _bindCollections = [];
 
         public DataBindingService Bind(INotifyPropertyChanged source, PropertyInfo sourceProperty, object target, PropertyInfo targetProperty, IValueConverter? valueConverter = null)
         {
@@ -45,7 +48,6 @@ namespace CoreServices.DataBinding
             targetProperty.SetValue(target, valueConverter.Convert(sourceProperty.GetValue(source)!, target.GetType(), null));
             return this;
         }
-
         public DataBindingService UnBind(INotifyPropertyChanged source, PropertyInfo sourceProperty, object target, PropertyInfo targetProperty)
         {
             if (_bindings.TryGetValue(source, out var values))
@@ -64,6 +66,52 @@ namespace CoreServices.DataBinding
                 if (values.Count == 0)
                 {
                     _bindings.Remove(source);
+                }
+            }
+            return this;
+        }
+        public DataBindingService BindCollection(
+            INotifyCollectionChanged collection,
+            Action<INotifyCollectionChanged, IList> itemsAdded,
+            Action<INotifyCollectionChanged, IList> itemsRemoved)
+        {
+            var actions = (itemsAdded, itemsRemoved);
+            if (_bindCollections.TryGetValue(collection, out var list))
+            {
+                list.Add(actions);
+            }
+            else
+            {
+                _bindCollections[collection] = [actions];
+                collection.CollectionChanged += OnCollectionChanged;
+            }
+            return this;
+        }
+        public DataBindingService UnBindCollection(
+            INotifyCollectionChanged collection)
+        {
+            if (_bindCollections.TryGetValue(collection, out var list))
+            {
+                collection.CollectionChanged -= OnCollectionChanged;
+                _bindCollections.Remove(collection);
+            }
+            return this;
+        }
+        public DataBindingService UnBindCollection(
+            INotifyCollectionChanged collection,
+            Action<INotifyCollectionChanged, IList> itemsAdded,
+            Action<INotifyCollectionChanged, IList> itemsRemoved)
+        {
+            if (_bindCollections.TryGetValue(collection, out var list))
+            {
+                if (list.Find(s => s.itemsAdded == itemsAdded && s.itemsRemoved == itemsRemoved) is var actions)
+                {
+                    list.Remove(actions);
+                    if (list.Count == 0)
+                    {
+                        collection.CollectionChanged -= OnCollectionChanged;
+                        _bindCollections.Remove(collection);
+                    }
                 }
             }
             return this;
@@ -87,9 +135,41 @@ namespace CoreServices.DataBinding
             }
         }
 
+        private void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (sender is null)
+                return;
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewItems is null)
+                        break;
+                    foreach(var action in _bindCollections[(sender as INotifyCollectionChanged)!])
+                    {
+                        action.itemsAdded((sender as INotifyCollectionChanged)!, e.NewItems);
+                    }
+
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldItems is null)
+                        break;
+                    foreach (var action in _bindCollections[(sender as INotifyCollectionChanged)!])
+                    {
+                        action.itemsRemoved((sender as INotifyCollectionChanged)!, e.OldItems);
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
         protected override void DestoryManagedResource()
         {
             _bindings.Clear();
+            _bindCollections.Clear();
         }
 
         protected override void DestoryUnmanagedResource()
---
+++ CoreServices/DataBinding/ValueConverters/StringToNumValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using CoreServices.DataBinding.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreServices.DataBinding.ValueConverters
+{
+    public class StringToNumValueConverter : IValueConverter
+    {
+        public object Convert(object sourceValue, Type targetType, object? parameter)
+        {
+            return int.Parse((string)sourceValue);
+        }
+    }
+}
